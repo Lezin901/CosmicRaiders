2658cbf2c422b3a80a72e10d6392f0cf8f9caf1f
Butterbanenbrot <jh.hagedorn@gmail.com>
2024-05-19 13:40:44 +0200

    added fighter explosion sound and merged stuff

--- core/src/com/cosmicraiders/Config.java
+++ core/src/com/cosmicraiders/Config.java
@@ -1,7 +1,7 @@
 package com.cosmicraiders;
 
 public class Config {
-    public static float volume = 0;
+    public static float volume = 0.3f;
 
     public static final int resolutionX = 1920;
     public static final int resolutionY = 1080;
