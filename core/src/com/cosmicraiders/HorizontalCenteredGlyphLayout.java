0b70dce04631b22c6899b9fcaf04a77b1eea5199
Butterbanenbrot <jh.hagedorn@gmail.com>
2024-07-09 15:20:15 +0200

    javadoc created

--- core/src/com/cosmicraiders/HorizontalCenteredGlyphLayout.java
+++ core/src/com/cosmicraiders/HorizontalCenteredGlyphLayout.java
@@ -3,6 +3,9 @@ package com.cosmicraiders;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 
+/**
+ * This class is a custom GlyphLayout which centers the text horizontally.
+ */
 public class HorizontalCenteredGlyphLayout extends GlyphLayout {
     private String text;
     private BitmapFont font;
@@ -17,6 +20,10 @@ public class HorizontalCenteredGlyphLayout extends GlyphLayout {
 
     }
 
+    /**
+     * Sets the text and centers it horizontally.
+     * @param text The text to be displayed.
+     */
     public void setText(String text) {
         this.text = text;
         this.setText(font, text);
